c6e0041753c06dd82032ff555775c382e8acf5f5
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-10-01 12:07:38 +0300

    Добавьте файлы проекта.

---
+++ Lesson4/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4
+{
+    class Car
+    {
+        int doors;
+        int enginePower;
+        string model;
+        //Методы
+        //Void не возвращает значения после завершения метода
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        //Метод с Int возвращает значения (целочисленные)
+        public int Method2()
+        {
+            var myInt = 10;
+            return myInt;
+        }
+
+        //Метод с аргументами (аргумент указывает на тип переменной для возврата, число int приобразуется в строку string)
+        public string Method3(int count)
+        {
+            return count.ToString();
+        }
+    }
+
+    struct CarStruct
+    {
+        int doors;
+        int enginePower;
+        string model;
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            var _struct = new CarStruct();
+
+            car.Method1();
+
+            Console.WriteLine(car.Method3(car.Method2()));
+        }
+    }
+}
+
